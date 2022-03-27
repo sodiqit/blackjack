c754709da6d3e8dbec2da0535ece9bc29e9fdd29
Daniil <daniil.petro3@gmail.com>
2022-03-27 14:49:19 +0500

    remove __init__ from View

--- modules/view/view.py
+++ modules/view/view.py
@@ -3,9 +3,6 @@ from modules.utils.constants import OBSERVER_MESSAGES
 
 
 class View(Observer):
-    def __init__(self) -> None:
-        pass
-
     def start_game(self) -> None:
         print('Приветствую! Готов сыграть в Blackjack?')
         input('Нажми любую кнопку чтобы продолжить: ')
